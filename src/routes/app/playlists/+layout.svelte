440ad5dd0ee462fe35f94e82d6dd1410dcc25321
Oliver Turner <oliverturner@users.noreply.github.com>
2023-09-26 19:30:17 +0100

    nav ideas

--- src/routes/app/playlists/+layout.svelte
+++ src/routes/app/playlists/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import type { LayoutData } from "./$types";
 
+	import { page } from "$app/stores";
 	import NavPage from "$lib/components/nav-page.svelte";
 
 	export let data: LayoutData;
+
+	$: currentPath = $page.url.pathname;
 </script>
 
 <NavPage>
 	<svelte:fragment slot="nav-items">
 		{#each data.playlists?.items ?? [] as item}
 			<li>
-				<a class="nav__item" href="/app/tracks/{item.id}">
+				<a
+					class="nav__item"
+					class:nav__item--active={currentPath === `/app/playlists/${item.id}`}
+					href="/app/playlists/{item.id}"
+				>
 					<img class="nav__item__cover" src={item.images.at(-1)?.url} alt="Cover art" />
 					<span class="nav__item__title">{item.name}</span>
 				</a>
@@ -24,7 +31,12 @@
 <style>
 	.nav__item {
 		grid-template-areas: "cover title";
+		grid-template-rows: 1fr;
 		align-items: center;
+
+		&.nav__item--active {
+			background-color: rgb(var(--color-secondary-900));
+		}
 	}
 
 	.nav__item__title {
--- src/routes/app/playlists/[id]/+page.svelte
+++ src/routes/app/playlists/[id]/+page.svelte
@@ -2,10 +2,18 @@
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	console.log({ playlist_page: JSON.stringify(data.playlist, null, 2) });
 </script>
 
 <h2>{data.playlist?.name}</h2>
 
+<!--
 {#each data.playlist?.images ?? [] as image}
 	<img src={image.url} alt="Cover" />
+{/each}
+ -->
+
+{#each data.playlist?.tracks.items ?? [] as item}
+	<p>{item.track.name}</p>
 {/each}
