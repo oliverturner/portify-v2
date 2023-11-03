e4fd245c5c63bd9194cd5c26465d247cae05c5af
Oliver Turner <oliverturner@users.noreply.github.com>
2023-11-03 17:54:42 +0000

    Tweak

--- src/routes/api/artists/following/+server.ts
+++ src/routes/api/artists/following/+server.ts
@@ -15,6 +15,7 @@ export async function GET({ locals, url }) {
 
 	if (!queryApi) return json(null);
 
+	// TODO: Automatically merge passed query params with defaults
 	const after = url.searchParams.get("after");
 	const limit = url.searchParams.get("limit") ?? apiParams.limit;
 
