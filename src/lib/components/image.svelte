4239673b3869d69a5957053b134f2343b09b93c0
Oliver Turner <oliverturner@users.noreply.github.com>
2023-12-01 17:56:39 +0000

    Define img.src on mount

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	export let src: string | undefined = "";
+	export let src: string | undefined;
 	export let alt: string;
 	export let square = true;
 
@@ -9,13 +9,15 @@
 	let loaded = false;
 
 	onMount(() => {
+		img.src = src ?? "/img/placeholder.svg";
+
 		img.onload = () => {
 			loaded = true;
 		};
 	});
 </script>
 
-<img {src} {alt} class:square class:loaded loading="lazy" bind:this={img} />
+<img {alt} class:square class:loaded loading="lazy" bind:this={img} />
 
 <style lang="postcss">
 	img {
