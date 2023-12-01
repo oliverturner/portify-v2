45db23de6216506c25a662f0ff56d7a956b807a6
Oliver Turner <oliverturner@users.noreply.github.com>
2023-12-01 16:41:15 +0000

    Stub credits page

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,1 +1,9 @@
-Credits
+<script lang="ts">
+	import type { LayoutData } from "../$types";
+
+	import App from "$lib/components/app.svelte";
+
+	export let data: LayoutData;
+</script>
+
+<App avatar={data.avatar}>Credits</App>
