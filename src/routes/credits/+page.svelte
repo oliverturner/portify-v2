507a5a0ad858bb2d7279786c34dba100bd48ea11
Oliver Turner <oliverturner@users.noreply.github.com>
2023-12-02 23:15:04 +0000

    Add vendor logos

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -12,27 +12,86 @@
 	<div class="content">
 		<h2>Credits</h2>
 		<div class="credits">
-			<div class="credit credit--intro">
+			<article class="credit credit--intro">
 				<p>
-					I made Portify not just to meet a need I had, but because I wanted to learn about new ways
-					of building apps.
+					I made Portify not just to meet a practical need, but because I wanted to learn about new
+					ways of building apps.
 				</p>
 				<p>
-					I'm grateful to the authors of the following libraries and frameworks for their work, and
-					for making it available to the community:
+					All these libraries and frameworks share a theme in common: a focus on web standards and
+					values like performance and accessibility; elegant, intuitive APIs; and a fresh take on
+					the developer experience.
 				</p>
-			</div>
-			<ul>
-				<li class="credit">
-					<h3>Spotify SDK</h3>
-				</li>
-				<li class="credit"><h3>SvelteKit</h3></li>
-				<li class="credit"><h3>Melt UI</h3></li>
-				<li class="credit"><h3>Lucia Auth</h3></li>
-				<li class="credit"><h3>Turso DB</h3></li>
-				<li class="credit"><h3>Drizzle ORM</h3></li>
-				<li class="credit"><h3>Open Props</h3></li>
-			</ul>
+				<p>
+					I'm immensely grateful to the authors for their work, and for making it available to the
+					community:
+				</p>
+			</article>
+
+			<article class="credit">
+				<header>
+					<enhanced:img
+						class="logo square loaded"
+						alt="Spotify logo"
+						src="../../../static/img/logos/spotify.png"
+					/>
+					<h3>Spotify TypeScript SDK</h3>
+				</header>
+				<p>
+					This build was spurred into existence by the announcement of the <a
+						href="https://developer.spotify.com/blog/2023-07-03-typescript-sdk"
+					>
+						Spotify TypeScript SDK
+					</a>
+					by my super-talented friend
+					<a href="https://bsky.app/profile/thisisjofrank.bsky.social">Jo Franchetti</a>.
+				</p>
+				<p>
+					The comprehensive type definitions didn't just make working with paged data a breeze but
+					actually inspired the super simple infinite scroll implementation. Fantastic work.
+				</p>
+			</article>
+
+			<article class="credit">
+				<header>
+					<img class="logo square loaded" alt="SvelteKit logo" src="/img/logos/svelte.svg" />
+					<h3>SvelteKit</h3>
+				</header>
+			</article>
+
+			<article class="credit">
+				<header>
+					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/melt-ui.svg" />
+					<h3>Melt UI</h3>
+				</header>
+			</article>
+
+			<article class="credit">
+				<header>
+					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/lucia.svg" />
+					<h3>Lucia Auth</h3>
+				</header>
+			</article>
+
+			<article class="credit">
+				<header>
+					<enhanced:img
+						class="logo square loaded"
+						alt="Spotify logo"
+						src="../../../static/img/logos/turso.png"
+					/>
+					<h3>Turso DB</h3>
+				</header>
+			</article>
+
+			<article class="credit">
+				<header>
+					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/drizzle-orm.svg" />
+					<h3>Drizzle ORM</h3>
+				</header>
+			</article>
+
+			<article class="credit"><h3>Open Props</h3></article>
 		</div>
 	</div>
 </App>
@@ -54,16 +113,32 @@
 		gap: 1rem;
 	}
 
-	ul {
-		display: contents;
-	}
-
 	.credit {
+		display: grid;
+		align-content: start;
+		gap: 1rem;
+
 		padding: 1rem;
 		background: var(--surface-4);
 
 		&.credit--intro {
-			grid-row: 1 / 3;
+			padding: 0;
+			background: none;
 		}
+
+		& header {
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+		}
+
+		& p a {
+			text-decoration: underline;
+		}
+	}
+
+	.logo {
+		width: 4rem;
+		height: 4rem;
 	}
 </style>
