612b7a8a0e985f0ca8fd600eaee34248167b1a83
Oliver Turner <oliverturner@users.noreply.github.com>
2023-12-03 23:18:46 +0000

    Add credit links

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -4,8 +4,6 @@
 	import App from "$lib/components/app.svelte";
 
 	export let data: PageData;
-
-	console.log({ data });
 </script>
 
 <App avatar={data.avatar}>
@@ -35,7 +33,11 @@
 						alt="Spotify logo"
 						src="../../../static/img/logos/spotify.png"
 					/>
-					<h3>Spotify TypeScript SDK</h3>
+					<h3>
+						<a href="https://developer.spotify.com/blog/2023-07-03-typescript-sdk">
+							Spotify TypeScript SDK
+						</a>
+					</h3>
 				</header>
 				<p>
 					This year's build was inspired by the announcement of the <a
@@ -55,28 +57,35 @@
 			<article id="svelte" class="credit">
 				<header>
 					<img class="logo square loaded" alt="SvelteKit logo" src="/img/logos/svelte.svg" />
-					<h3>SvelteKit</h3>
+					<h3>
+						<a href="https://kit.svelte.dev/">SvelteKit</a>
+					</h3>
 				</header>
 				<p>
-					I genuinely enjoy working with Svelte and SvelteKit: the combination makes the challenging
-					easy and the features just keep getting better (hello Runes!).
+					I genuinely love working with <a href="https://svelte.dev/">Svelte</a> and
+					<a href="https://kit.svelte.dev/">SvelteKit</a>: the combination makes the challenging
+					easy and the features just keep getting better (hello
+					<a href="https://svelte.dev/blog/runes">Runes!</a>).
 				</p>
 				<p>
-					As much as I love Astro, and as much as I want to keep exploring options like Enhance, for
-					an app like Portify it's genuinely difficult to imagine any other combination being as
-					rewarding.
+					As much as I love <a href="https://astro.build/">Astro</a>, and as much as I want to keep
+					exploring options like <a href="https://enhance.dev/">Enhance</a>, for an app like Portify
+					it's genuinely difficult to imagine any other combination being as rewarding.
 				</p>
 			</article>
 
 			<article id="melt-ui" class="credit">
 				<header>
 					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/melt-ui.svg" />
-					<h3>Melt UI</h3>
+					<h3>
+						<a href="https://www.melt-ui.com/">Melt UI</a>
+					</h3>
 				</header>
 				<p>
-					I'm delighted to see a component library matching Radix in quality brought to Svelte. Melt
-					UI's builder pattern combines power with simplicity and the components are built with
-					accessibility as a primary concern. Really impressive stuff.
+					I'm delighted to see a component library matching Radix in quality brought to Svelte.
+					<a href="https://www.melt-ui.com/">Melt UI</a>'s builder pattern combines power with
+					simplicity and the components are built with accessibility as a primary concern. Really
+					impressive stuff.
 				</p>
 				<p>Bonus: being a headless UI library, it doesn't impose Tailwind on me.</p>
 			</article>
@@ -84,11 +93,13 @@
 			<article id="lucia-auth" class="credit">
 				<header>
 					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/lucia.svg" />
-					<h3>Lucia Auth</h3>
+					<h3>
+						<a href="https://lucia-auth.com/">Lucia Auth</a>
+					</h3>
 				</header>
 				<p>
 					A library I just know I'll keep coming back to. OAuth may be more fiddly than hard, but
-					Lucia does an amazing job of making the fiddly simple.
+					<a href="https://lucia-auth.com/">Lucia</a> does an amazing job of making the fiddly simple.
 				</p>
 				<p>
 					The radical minimalism means that you're responsible for managing and refreshing your
@@ -101,14 +112,17 @@
 				<header>
 					<enhanced:img
 						class="logo square loaded"
-						alt="Spotify logo"
+						alt="Turso logo"
 						src="../../../static/img/logos/turso.png"
 					/>
-					<h3>Turso DB</h3>
+					<h3>
+						<a href="https://turso.tech/">Turso DB</a>
+					</h3>
 				</header>
 				<p>
-					The convergence of Edge Compute and libSQL's file-as-db architecture means Turso can offer
-					500 databases at the free tier. Just phenomenal.
+					The convergence of Edge Compute and libSQL's file-as-db architecture means
+					<a href="https://turso.tech/">Turso</a>
+					can offer 500 databases at the free tier. Just phenomenal.
 				</p>
 				<p>
 					The perfect pairing for Lucia, and so easy to work with that it's almost hard to resist
@@ -118,24 +132,29 @@
 
 			<article id="drizzle-orm" class="credit">
 				<header>
-					<img class="logo square loaded" alt="Lucia logo" src="/img/logos/drizzle-orm.svg" />
-					<h3>Drizzle ORM</h3>
+					<img class="logo square loaded" alt="Drizzle ORM logo" src="/img/logos/drizzle-orm.svg" />
+					<h3>
+						<a href="https://orm.drizzle.team/">Drizzle ORM</a>
+					</h3>
 				</header>
 				<p>
 					Real talk: the lack of database migration tooling for Node that even remotely matched what
 					existed as standard for PHP, Python or Ruby was a major drag on me building db-backed
 					projects in the past.
 				</p>
-				<p>I'm so glad I found Drizzle 🥰</p>
+				<p>I'm so glad I found <a href="https://orm.drizzle.team/">Drizzle</a> 🥰</p>
 			</article>
 
 			<article id="open-props" class="credit">
 				<header>
 					<img class="logo square loaded" alt="Open Props logo" src="/img/logos/open-props.svg" />
-					<h3>Open Props</h3>
+					<h3>
+						<a href="https://open-props.style/">Open Props</a>
+					</h3>
 				</header>
 				<p>
-					Adam Argyle's ever-expanding toolbox for CSS gives developers super powers via the medium
+					Adam Argyle's ever-expanding sub-atomic toolbox for CSS,
+					<a href="https://open-props.style/">Open Props</a> gives developers super powers via the medium
 					of Custom Properties.
 				</p>
 				<p>
